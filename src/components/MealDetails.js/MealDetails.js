3f54599c9e59d221d38ebb598d13576d2c76a1d6
sheikhlimon <sheikhlimon404@gmail.com>
2021-03-15 02:04:01 +0600

    add meal db api search

---
+++ src/components/MealDetails.js/MealDetails.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const MealDetails = () => {
+    const [meal, setMeal] = useState({});
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MealDetails;
